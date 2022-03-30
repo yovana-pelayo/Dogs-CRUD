0758e834eec1907f65a5db314bd93a674c4e42f9
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-30 14:51:06 -0700

    added laoding state to detailspage

--- src/views/DogDetail.js
+++ src/views/DogDetail.js
@@ -5,13 +5,16 @@ import { fetchDogById } from '../services/dogs';
 export default function DogDetail() {
   const params = useParams();
   const [dog, setDog] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchDogById(params.id);
       setDog(data);
+      setLoading(false);
     };
     fetchData();
   }, [params.id]);
+  if (loading) return <div>LOADING</div>;
   return (
     <div>
       <h1>Fuzzy Buddies Details!</h1>
